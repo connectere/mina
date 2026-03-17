00527e325b9484fd585d9f82983aa1109cee39e0
bfb <bfbohn@protonmail.com>
2026-03-17 15:16:38 -0300

    Update bundler

--- tasks/mina/bundler.rb
+++ tasks/mina/bundler.rb
@@ -5,13 +5,15 @@ require 'mina/default'
 set :bundle_bin, 'bundle'
 set :bundle_path, 'vendor/bundle'
 set :bundle_withouts, 'development test'
-set :bundle_options, -> { %(--without #{fetch(:bundle_withouts)} --path "#{fetch(:bundle_path)}" --deployment) }
 set :shared_dirs, fetch(:shared_dirs, []).push(fetch(:bundle_path))
 
 namespace :bundle do
   desc 'Install gem dependencies using Bundler.'
   task :install do
     comment %(Installing gem dependencies using Bundler)
+    command %(bundle config set deployment true)
+    command %(bundle config set path "#{fetch(:bundle_path)}")
+    command %(bundle config set without #{fetch(:bundle_withouts)})
     command %(#{fetch(:bundle_bin)} install #{fetch(:bundle_options)})
   end
 
